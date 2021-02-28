5f93937cec087e94ef2a836ff11aae66a9346ae0
Drew Cook <drewcook29@gmail.com>
2021-02-28 15:40:17 -0700

    joi package typos

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const { User } = require('../models/User');
-const Joi = require('Joi');
+const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
